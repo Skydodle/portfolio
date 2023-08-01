98a1f9acb1e6cb18bc8c2a1896c3606a89cf4b6f
Johnny Wu <skydodle@gmail.com>
2023-07-31 17:09:17 -0700

    Updated skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -15,7 +15,7 @@ export const Skills = () => {
     },
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 3,
+      items: 4,
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
@@ -41,8 +41,150 @@ export const Skills = () => {
                 className='skill-slider'
               >
                 <div className='item'>
-                  <img src={meter1} alt='skills' />
+                  <img
+                    src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg'
+                    alt='skills'
+                  />
                   <h5>Javascript</h5>
+                </div>
+                <div className='item'>
+                  <img
+                    src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-plain.svg'
+                    alt='skills'
+                  />
+                  <h5>Typescript</h5>
+                </div>
+                <div className='item'>
+                  <img
+                    src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg'
+                    alt='skills'
+                  />
+                  <h5>React</h5>
+                </div>
+                <div className='item'>
+                  <i class='devicon-nextjs-original font-icons'></i>
+                  <h5>NextJS</h5>
+                </div>
+                <div className='item'>
+                  <img
+                    src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg'
+                    alt='skills'
+                  />
+                  <h5>Tailwind</h5>
+                </div>
+                <div className='item'>
+                  <img src={meter1} alt='skills' />
+                  <h5>Styled-Components</h5>
+                </div>
+                <div className='item'>
+                  <img
+                    src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg'
+                    alt='skills'
+                  />
+                  <h5>Node</h5>
+                </div>
+                <div className='item'>
+                  <i class='devicon-express-original font-icons'></i>
+                  <h5>Express</h5>
+                </div>
+                <div className='item'>
+                  <img src={meter1} alt='skills' />
+                  <h5>RESTful API</h5>
+                </div>
+                <div className='item'>
+                  <img
+                    src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nestjs/nestjs-plain.svg'
+                    alt='skills'
+                  />
+                  <h5>NestJS</h5>
+                </div>
+                <div className='item'>
+                  <img
+                    src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/graphql/graphql-plain.svg'
+                    alt='skills'
+                  />
+                  <h5>GraphQL</h5>
+                </div>
+                <div className='item'>
+                  <img
+                    src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg'
+                    alt='skills'
+                  />
+                  <h5>PostgreSQL</h5>
+                </div>
+                <div className='item'>
+                  <img
+                    src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg'
+                    alt='skills'
+                  />
+                  <h5>MongoDB</h5>
+                </div>
+                <div className='item'>
+                  <img
+                    src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg'
+                    alt='skills'
+                  />
+                  <h5>MySQL</h5>
+                </div>
+                <div className='item'>
+                  <i class='devicon-github-original font-icons'></i>
+                  <h5>Github</h5>
+                </div>
+                <div className='item'>
+                  <img
+                    src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg'
+                    alt='skills'
+                  />
+                  <h5>Git</h5>
+                </div>
+                <div className='item'>
+                  <img
+                    src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-plain.svg'
+                    alt='skills'
+                  />
+                  <h5>Docker</h5>
+                </div>
+                <div className='item'>
+                  <img
+                    src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nginx/nginx-original.svg'
+                    alt='skills'
+                  />
+                  <h5>NginX</h5>
+                </div>
+                <div className='item'>
+                  <img
+                    src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-plain-wordmark.svg'
+                    alt='skills'
+                  />
+                  <h5>AWS EC2</h5>
+                </div>
+                <div className='item'>
+                  <img
+                    src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bitbucket/bitbucket-original.svg'
+                    alt='skills'
+                  />
+                  <h5>Bitbucket</h5>
+                </div>
+                <div className='item'>
+                  <img
+                    src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg'
+                    alt='skills'
+                  />
+                  <h5>Figma</h5>
+                </div>
+                <div className='item'>
+                  <img
+                    src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg'
+                    alt='skills'
+                  />
+                  <h5>HTML5</h5>
+                </div>
+                <div className='item'>
+                  <img
+                    src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg'
+                    alt='skills'
+                  />
+                  <h5>CSS3</h5>
                 </div>
                 <div className='item'>
                   <img src={meter1} alt='skills' />
@@ -53,40 +195,8 @@ export const Skills = () => {
                   <h5>Responsive Design</h5>
                 </div>
                 <div className='item'>
-                  <img src={meter3} alt='skills' />
-                  <h5>Typescript</h5>
-                </div>
-                <div className='item'>
-                  <img src={meter3} alt='skills' />
-                  <h5>React</h5>
-                </div>
-                <div className='item'>
-                  <img src={meter1} alt='skills' />
-                  <h5>Restful APIs</h5>
-                </div>
-                <div className='item'>
                   <img src={meter2} alt='skills' />
-                  <h5>Tailwind</h5>
-                </div>
-                <div className='item'>
-                  <img src={meter2} alt='skills' />
-                  <h5>Styled-Components</h5>
-                </div>
-                <div className='item'>
-                  <img src={meter3} alt='skills' />
-                  <h5>Node</h5>
-                </div>
-                <div className='item'>
-                  <img src={meter2} alt='skills' />
-                  <h5>GraphQL</h5>
-                </div>
-                <div className='item'>
-                  <img src={meter3} alt='skills' />
-                  <h5>PostgreSQL</h5>
-                </div>
-                <div className='item'>
-                  <img src={meter2} alt='skills' />
-                  <h5>MongoDB</h5>
+                  <h5>Microservices</h5>
                 </div>
               </Carousel>
             </div>
