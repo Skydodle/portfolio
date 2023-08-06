7aae6b1a30b325bfa61141ab4374b4e21ea73036
Johnny Wu <skydodle@gmail.com>
2023-08-05 20:26:07 -0700

    Replaced newsletter with resume

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 import { Container, Row, Col } from 'react-bootstrap';
-import { MailchimpForm } from './MailchimpForm';
 import linkedinIcon from '../assets/img/nav-icon1.svg';
 import githubIcon from '../assets/img/nav-icon2.svg';
 import instagramIcon from '../assets/img/nav-icon3.svg';
+import { DownloadResume } from './Resume';
 
 const SOCIAL_LINKS = [
   { url: 'https://www.linkedin.com/in/johnnyhcwu/', icon: linkedinIcon },
@@ -15,7 +15,7 @@ export const Footer = () => {
     <footer className='footer'>
       <Container>
         <Row className='align-items-center'>
-          <MailchimpForm />
+          <DownloadResume />
           <Col size={12} sm={6}>
             <span className='logo'>JW</span>
           </Col>
